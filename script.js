cdb747f6064967ee5f8e90ffbeab5e6ac39b2349
shaniakarim <156203203+shaniakarim@users.noreply.github.com>
2024-10-01 00:30:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,69 @@
-
-const map = L.map('map').setView([65, 25], 5);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-fetch('https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326')
-    .then(response => response.json())
-    .then(data => {
-   
-        const geoJsonLayer = L.geoJson(data, {
-            style: {
-                weight: 2
-            },
-            onEachFeature: (feature, layer) => {
-                layer.bindTooltip(feature.properties.name); 
-                layer.on('click', () => {
-                    
-                    Promise.all([
-                        fetch('https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f').then(res => res.json()),
-                        fetch('https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e').then(res => res.json())
-                    ]).then(([posData, negData]) => {
-                        const posMigration = posData[feature.properties.name] || 0;
-                        const negMigration = negData[feature.properties.name] || 0;
-                        const popupContent = `
-                            <strong>${feature.properties.name}</strong><br>
-                            Positive Migration: ${posMigration}<br>
-                            Negative Migration: ${negMigration}
-                        `;
-                        layer.bindPopup(popupContent).openPopup();
-                    });
-                });
-            }
-        }).addTo(map);
-
-        
-        map.fitBounds(geoJsonLayer.getBounds());
+// Check if the document is loaded
+if (document.readyState !== "loading") {
+    console.log("Document is ready!");
+    getData();
+  } else {
+    document.addEventListener("DOMContentLoaded", function () {
+      console.log("Document is loading!");
+      getData();
     });
-
-
-function getColor(pos, neg) {
-    const hue = Math.min((pos / neg) ** 3 * 60, 120);
-    return `hsl(${hue}, 75%, 50%)`;
-}
-function style(feature) {
-    const posMigration = 0; 
-    const negMigration = 1; V
+  }
+  
+  // Fetch the main map data
+  async function getData() {
+    let url = await fetch("https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326");
+    let data = await url.json();
+    loadMap(data);
+  }
+  
+  // Fetch migration data
+  async function getMigrationData() {
+    let posMigration = await fetch("https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f");
+    let negMigration = await fetch("https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e");
+  
+    let posData = await posMigration.json();
+    let negData = await negMigration.json();
+  
     return {
-        color: getColor(posMigration, negMigration),
-        weight: 2
+      posIndex: posData.dataset.dimension.Tuloalue.category.index,
+      posValues: posData.dataset.value,
+      negIndex: Object.values(negData.dataset.dimension)[0].category.index,
+      negValues: negData.dataset.value,
     };
-}
+  }
+  
+  // Simple color calculation
+  function getColor(pos, neg) {
+    let ratio = pos / neg;
+    let hue = Math.min(ratio ** 3 * 60, 120);
+    return `hsl(${hue}, 75%, 50%)`;
+  }
+  
+  // Initialize and display map
+  async function loadMap(geoData) {
+    let map = L.map("map").setView([65.0121, 25.4651], 5);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap contributors",
+    }).addTo(map);
+  
+    let migrationData = await getMigrationData();
+  
+    L.geoJSON(geoData, {
+      onEachFeature: function (feature, layer) {
+        let kuntaCode = feature.properties.kunta;
+        let posMigration = migrationData.posValues[migrationData.posIndex["KU" + kuntaCode]] || 0;
+        let negMigration = migrationData.negValues[migrationData.negIndex["KU" + kuntaCode]] || 0;
+  
+        let color = getColor(posMigration, negMigration);
+        layer.setStyle({ color: color, fillColor: color });
+  
+        layer.bindTooltip(feature.properties.nimi);
+        layer.on("click", function () {
+          layer.bindPopup(
+            `<b>${feature.properties.nimi}</b><br>Positive: ${posMigration}<br>Negative: ${negMigration}`
+          ).openPopup();
+        });
+      },
+    }).addTo(map);
+  }
+  
